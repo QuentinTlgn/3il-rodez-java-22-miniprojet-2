045e2e4fd11e0b8c523bf9d702932ad0b4390c7c
QuentinTlgn <tauleigq@3il.fr>
2024-02-28 09:34:49 +0100

    Ajout : Classe Noeud.java

---
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -0,0 +1,40 @@
+package fr.ecole3il.rodez2023.carte.elements;
+
+import java.util.List;
+
+/**
+ * Cette classe représente un nœud dans un graphe générique.
+ *
+ * @param <E> Le type de la valeur stockée dans le nœud.
+ */
+public class Noeud<E> {
+    private E valeur;
+    private List<Noeud<E>> voisins;
+    
+    /**
+     * Obtient la valeur stockée dans le nœud.
+     *
+     * @return La valeur du nœud.
+     */
+    public E getValeur() {
+        return valeur;
+    }
+
+    /**
+     * Obtient la liste des nœuds voisins du nœud actuel.
+     *
+     * @return La liste des nœuds voisins.
+     */
+    public List<Noeud<E>> getVoisins() {
+        return voisins;
+    }
+
+    /**
+     * Ajoute un nœud voisin au nœud actuel.
+     *
+     * @param voisin Le nœud voisin à ajouter.
+     */
+    public void ajouterVoisin(Noeud<E> voisin) {
+        voisins.add(voisin);
+    }
+}
