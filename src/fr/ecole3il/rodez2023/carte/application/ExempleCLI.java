f4ec7e53d9193ce5bf4d4552f8b55ea68d75aaec
QuentinTlgn <tauleigq@3il.fr>
2024-03-28 21:32:47 +0100

    Implémentation de l'adaptateur dans le ExempleCLI

--- src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
+++ src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
@@ -1,30 +1,30 @@
 package fr.ecole3il.rodez2023.carte.application;
 
 import fr.ecole3il.rodez2023.carte.AdaptateurAlgorithme;
-import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeAEtoile;
+
 import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeChemin;
-import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeDijkstra;
+import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeDjikstra;
 import fr.ecole3il.rodez2023.carte.elements.Carte;
+import fr.ecole3il.rodez2023.carte.elements.Case;
 import fr.ecole3il.rodez2023.carte.elements.Chemin;
 import fr.ecole3il.rodez2023.carte.manipulateurs.GenerateurCarte;
 
 /**
  * @author p.roquart
- * genre on peut tester le code
- * c'est agaçant tous ces tests
- * 
- * de toute façon quand ça compile c'est bon
+ *genre on peut tester le code
+ *c'est agaçant tous ces tests
+ *
+ *de toute façon quand ça compile c'est bon
  */
 public class ExempleCLI {
 
 	public static void main(String[] args) {
 		GenerateurCarte generateur = new GenerateurCarte();
-		Carte test = generateur.genererCarte(100,100);
-		AlgorithmeChemin algoChemin = new AlgorithmeDijkstra();
-		Chemin chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		Carte test = generateur.genererCarte(100, 100);
+		AlgorithmeChemin<Case> algorithmeChemin = new AlgorithmeDjikstra<>();
+		Chemin chemin = AdaptateurAlgorithme.trouverChemin(algorithmeChemin, test, 0, 0, 50, 50);
 		chemin.afficherChemin();
-		chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		chemin = AdaptateurAlgorithme.trouverChemin(algorithmeChemin, test, 0, 0, 50, 50);
 		chemin.afficherChemin();
 	}
-	
 }
